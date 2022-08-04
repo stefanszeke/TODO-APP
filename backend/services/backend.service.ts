bca23615ac3053f11c20db829f6053a96586f178
Stefan Szekely <sekestefan@gmail.com>
2022-08-04 20:51:59 +0200

    setup: debug setup for backend

--- backend/services/backend.service.ts
+++ backend/services/backend.service.ts
@@ -3,7 +3,7 @@ export default class BackendService {
 
   public setEnvironment(): string | undefined {
     if (process.env.NODE_ENV === 'production') return "users";
-    if (process.env.NODE_ENV === 'test') return "users_testing";
+    if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'debug') return "users_testing";
   }
 
 }
